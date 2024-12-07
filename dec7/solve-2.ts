1cf937bbac4f465a7de5df9e9def25ab485544e6
Matthijs Groen <matthijs.groen@gmail.com>
2024-12-07 09:54:35 +0100

    Dec 7

---
+++ dec7/solve-2.ts
@@ -0,0 +1,46 @@
+import { readFileSync } from "bun:fs";
+
+const data: string[] = readFileSync("./data.txt", "utf8").split("\n");
+
+type Formula = {
+  answer: number;
+  elements: number[];
+};
+
+const parsedData: Formula[] = data.map((line) => {
+  const [answer, elements] = line.split(": ");
+  return {
+    answer: Number(answer),
+    elements: elements.split(" ").map(Number),
+  };
+});
+
+const canBeSolved = (formula: Formula): boolean => {
+  const operatorsNeeded = formula.elements.length - 1;
+  const possibleSolutions = Math.pow(3, operatorsNeeded);
+
+  for (let i = 0; i < possibleSolutions; i++) {
+    const binary = i.toString(3).padStart(operatorsNeeded, "0").split("");
+
+    const calculatedAnswer = formula.elements.reduce((acc, element, index) => {
+      const operator = binary.shift();
+      if (operator === "0") {
+        return acc + element;
+      } else if (operator === "1") {
+        return acc * element;
+      } else {
+        return Number(`${acc}${element}`);
+      }
+    });
+    if (calculatedAnswer === formula.answer) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const result = parsedData.filter(canBeSolved).reduce((acc, formula) => {
+  return acc + formula.answer;
+}, 0);
+
+console.log(result);
